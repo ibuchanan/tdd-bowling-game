bf4f265196c99f36ca8e7c1b4bdb54946256183f
Ian Buchanan <ian.buchanan@versionone.com>
2012-05-04 14:20:21 -0400

    From Greg Johnson pairing session.

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Bowling;
 using Bowling.Specs.Infrastructure;
 
 namespace specs_for_bowling
@@ -16,5 +18,56 @@ namespace specs_for_bowling
 		{
 			_itWorked.should_be_true("we're ready to roll!");
 		}
+
+        [Specification]
+        public void InitialScoreIsZeroTest()
+        {
+            var target = new Game();
+            target.Score().should_equal(0);
+        }
+
+        [Specification]
+        public void FrameOneLessThanTenTest()
+        {
+            var target = new Game();
+            target.Roll(1);
+            target.Roll(4);
+            target.Score().should_equal(5);
+        }
+
+        [Specification]
+        public void InitializeGameTest()
+        {
+            var target = new Game(new [] {1, 4});
+            target.Score().should_equal(5);
+        }
+
+        [Specification]
+        public void FrameTwoScoreTest()
+        {
+            var target = new Game(new[] { 1, 4 });
+            target.Roll(4);
+            target.Roll(5);
+            target.Score().should_equal(14);
+        }
+
+        [Specification]
+        public void FrameThreeScoreTest()
+        {
+            var target = new Game(new[] { 1, 4, 4, 5 });
+            target.Roll(6);
+            target.Roll(4);
+            target.Score().should_equal(24);
+        }
+
+        [Specification]
+        public void FrameFourScoreTest()
+        {
+            var target = new Game(new[] {1, 4, 4, 5, 6, 4});
+            target.Roll(5);
+            target.Roll(5);
+            target.Score().should_equal(44);
+        }
+
 	}
 }
---
+++ Source/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+
+    public class Game
+    {
+
+        private readonly IList<int> _pinLog = new List<int>();
+        private int _score;
+
+
+        public Game(IEnumerable<int> initialGameState)
+        {
+            _pinLog = new List<int>(initialGameState);
+        }
+
+        public Game(){}
+
+        public void Roll(int pins)
+        {
+            if (IsSpare())
+            {
+                _score += pins*2;
+            }
+            else
+            {
+                _score += pins;
+            }
+            _pinLog.Add(pins);
+        }
+
+        private bool IsSpare()
+        {
+            var lastItem = (_pinLog.Count - 1);
+            var secondToLastItem = (_pinLog.Count - 2);
+            return (_pinLog[lastItem] + _pinLog[secondToLastItem]) == 10;
+        }
+
+        public int Score()
+        {
+            if (IsSpare())
+            return _pinLog.Sum();
+        }
+
+    }
+}
